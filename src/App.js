9b6c8c35badafcb2e0ec9091364964f005273281
kimo722504 <kimo722504@gmail.com>
2019-04-26 20:54:21 +0900

    props개념익혔고 리팩토링 했음

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Subject from "./components/Subject";
+import Content from "./components/Content";
+import TOC from "./components/TOC";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC desc="html"></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
