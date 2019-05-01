1536e8f4772cf7402e0a59dcd83c6fcf544f9210
kimo722504 <kimo722504@gmail.com>
2019-05-01 17:59:32 +0900

    state,bind,setState 이해하기

--- src/App.js
+++ src/App.js
@@ -12,41 +12,52 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      mode: 'welcome',
+      welcome: { title: 'WEB', sub: 'World wide Web!' },
       subject: {
         title: "WEB",
         sub: "world wide web!"
       },
       contents: [
         {
-          id: 1,
-          title: "HTML",
-          desc: "html is for information"
+          id: 1, title: "HTML", desc: "html is for information"
         },
         {
-          id: 2,
-          title: "CSS",
-          desc: "css is for design"
+          id: 2, title: "CSS", desc: "css is for design"
         },
         {
-          id: 3,
-          title: "Javascript",
-          desc: "javascript is for interactivte"
+          id: 3, title: "Javascript", desc: "javascript is for interactivte"
         },
         {
-          id: 4,
-          title: "React",
-          desc: "react is for reuse"
+          id: 4, title: "React", desc: "react is for reuse"
         }
       ]
 
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.sub;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
+    console.log("App render");
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
